d657a1f2a3311e19bd575f28db3be136f651b3b2
Kalle Struik <kalle@kallestruik.nl>
2021-02-19 09:05:09 +0100

    Make it check a 200x200 area around 0,0 for the best 16x16 to mine.

--- src/main/java/eutro/seed_chunk_checker/SeedChunkChecker.java
+++ src/main/java/eutro/seed_chunk_checker/SeedChunkChecker.java
@@ -3,79 +3,114 @@ package eutro.seed_chunk_checker;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import com.google.gson.stream.JsonWriter;
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
-import net.minecraft.block.Block;
 import net.minecraft.server.Main;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.dedicated.AbstractPropertiesHandler;
 import net.minecraft.server.dedicated.ServerPropertiesLoader;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
-import net.minecraft.world.chunk.WorldChunk;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.lang.reflect.Field;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.ToIntFunction;
 
 public class SeedChunkChecker {
+    // You should really keep these two equal.
+    private static final int X_RANGE = 100;
+    private static final int Z_RANGE = 100;
+
     public static void main(String[] args) {
-        int x = Integer.parseInt(args[1]);
-        int y = Integer.parseInt(args[2]);
-        File file = new File(args[0]);
+        String seed = args[0];
+        File file = new File(seed);
         if (!file.mkdir()) {
-            System.err.printf("Directory %s already exists (seed may have already been checked)%n", args[0]);
+            System.err.printf("Directory %s already exists (seed may have already been checked)%n", seed);
         }
-        setSeed(args[0], file);
-        try (MinecraftServer server = startDedicatedServer("--nogui", "--universe", args[0])) {
+        setSeed(seed);
+        try {
+            MinecraftServer server = startDedicatedServer("--nogui", "--world", seed);
             ServerWorld world;
             while ((world = server.getWorld(World.OVERWORLD)) == null) {
                 //noinspection BusyWait - cry about it
                 Thread.sleep(0);
             }
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            JsonObject obj = new JsonObject();
-            obj.addProperty("x", x);
-            obj.addProperty("y", y);
-            obj.add("frequencies", checkChunk(world, x, y));
-            try (JsonWriter writer = gson.newJsonWriter(new FileWriter(new File(file, "frequency.json")))) {
-                gson.toJson(obj, writer);
+            JsonObject data = checkWorld(world);
+            try (JsonWriter writer = gson.newJsonWriter(new FileWriter(new File(file, "data.json")))) {
+                gson.toJson(data, writer);
+                writer.flush();
             }
+
+            server.stop(true);
         } catch (Exception ignored) {
         }
-        System.exit(0);
+
+        try {
+//            // This is beautiful.
+//            String pid = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+//            Runtime.getRuntime().exec("kill -9 " + pid);
+        } catch (Exception youAreDeadAnyway) {}
     }
 
-    private static JsonObject checkChunk(ServerWorld world, int x, int y) {
-        Object2IntMap<Block> counts = new Object2IntOpenHashMap<>();
-        ChunkPos chunkPos = new ChunkPos(x, y);
-        WorldChunk chunk = world.getChunk(x, y);
-        for (BlockPos pos : BlockPos.iterate(
-                chunkPos.toBlockPos(0, 0, 0),
-                chunkPos.toBlockPos(15, 255, 15)
-        )) {
-            Block state = chunk.getBlockState(pos).getBlock();
-            counts.put(state, counts.getInt(state) + 1);
+    private static JsonObject checkWorld(ServerWorld world) {
+        System.out.println(world.getSeed());
+        int[][] grid = new int[X_RANGE*2+1][Z_RANGE*2+1];
+        for (int x = -X_RANGE; x <= X_RANGE; x++) {
+            for (int z = -Z_RANGE; z <= Z_RANGE; z++) {
+                int total = 0;
+
+                for (int y = 5; y <= 80; y++) {
+                    String block = Registry.BLOCK.getId(world.getBlockState(new BlockPos(x, y, z)).getBlock()).toString();
+                    if (block.equals("minecraft:air") || block.equals("minecraft:cave_air"))
+                        continue;
+
+                    total++;
+                }
+                grid[x+X_RANGE][z+Z_RANGE] = total;
+            }
         }
+
+        return bestArea(grid);
+    }
+
+    public static JsonObject bestArea(int[][] list){
+        int totalLines = X_RANGE * 2;
+        int sideLength = 16;
+        int bestAreaNum = Integer.MAX_VALUE;
+        int[] bestCoords = new int[2];
+        for (int x = 0; x < totalLines-sideLength+1; x++) {
+            for (int y = 0; y < totalLines-sideLength+1; y++) {
+                int total = 0;
+                for (int i = x; i < x+sideLength; i++) {
+                    for (int j = y; j < y+sideLength; j++) {
+                        total+= list[i][j];
+                    }
+                }
+                if(total < bestAreaNum){
+                    bestAreaNum = total;
+                    bestCoords[0] = x-(totalLines / 2);
+                    bestCoords[1] = y-(totalLines / 2);
+                }
+
+            }
+        }
+        System.out.println(bestAreaNum);
         JsonObject obj = new JsonObject();
-        counts.object2IntEntrySet()
-                .stream()
-                .sorted(Comparator.comparingInt((ToIntFunction<? super Object2IntMap.Entry<?>>) Object2IntMap.Entry::getIntValue).reversed())
-                .forEachOrdered(entry -> obj.addProperty(Registry.BLOCK.getId(entry.getKey()).toString(), entry.getIntValue()));
+        obj.add("area", new JsonPrimitive(bestAreaNum));
+        obj.add("x", new JsonPrimitive(bestCoords[0]));
+        obj.add("z", new JsonPrimitive(bestCoords[1]));
+        System.out.println(obj);
         return obj;
     }
 
-    private static void setSeed(String seed, File file) {
-        ServerPropertiesLoader properties = new ServerPropertiesLoader(new File(file, "server.properties").toPath());
+    private static void setSeed(String seed) {
+        ServerPropertiesLoader properties = new ServerPropertiesLoader(new File("server.properties").toPath());
         Properties props;
         try {
             Field propertiesField = AbstractPropertiesHandler.class.getDeclaredField("properties");
